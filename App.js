b0031671f522978932e44b19673604b003eceb5e
MuhammadUsman <M.UsmanQau786@gmail.com>
2020-10-15 19:58:41 +0500

    Navigation added

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import HomeScreen from "./Containers/HomeScreen";
+import {navigationRef} from "./Utilities/NavigatorServices";
+import FlipCoin from "./Containers/FlipCoin";
+import RandomNumbers from "./Containers/RandomNumbers";
+import RollDice from "./Containers/Rolldice";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+const Stack = createStackNavigator();
+
+function App() {
+	return (
+		<NavigationContainer ref={ navigationRef }>
+			<Stack.Navigator>
+				<Stack.Screen name="Home"  component={ HomeScreen }/>
+				<Stack.Screen name="RollDiceScreen" options={{title:'Roll Dice'}} component={ RollDice }/>
+				<Stack.Screen name="FlipCoinScreen" options={{title:'Flip Coin'}} component={ FlipCoin }/>
+				<Stack.Screen name="RandomNumbersScreen" options={{title:'Random Numbers'}} component={ RandomNumbers }/>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
